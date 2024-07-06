264d9acbcf0e26c7354ded0db9f3b4fd2f719c21
Okerew <93822247+Okerew@users.noreply.github.com>
2024-07-06 18:38:57 +0200

    Create __about__.py

---
+++ src/__about__.py
@@ -0,0 +1,20 @@
+__title__ = 'okrolearn'
+__description__ = 'A machine learning library by Okerew, which is a raw implementation of combining scikit-learn and pytorch'
+__url__ = 'https://github.com/Okerew/okrolearn'
+__version__ = '0.2.1'
+__author__ = 'Okerew'
+__author_email__ = 'okerewgroup@proton.me'
+__license__ = 'MIT'
+__keywords__ = 'keyword1, keyword2, keyword3'
+__classifiers__ = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+]
